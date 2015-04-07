a8b26078cb70bff7200d8268059faa9e339d7c7b
Yuriy Nemtsov <nemtsov@gmail.com>
2015-04-07 15:39:05 -0400

    tests: fix jsdom window issue for 0.12

--- test/util/test-helper.js
+++ test/util/test-helper.js
@@ -9,5 +9,5 @@
 var jsdom = require('jsdom');
 
 global.document = jsdom.jsdom('<html><body></body></html>');
-global.window = document.parentWindow;
+global.window = document.defaultView;
 global.navigator = window.navigator;
